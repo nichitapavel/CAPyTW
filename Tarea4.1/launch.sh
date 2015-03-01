fdc27a173c1c3be05073399423b93dc96ffcc7ed
Pavel <nichitapavel@live.com>
2015-03-01 11:02:18 +0000

    se ejecuta con ./launch > data.txt

--- Tarea4.1/launch.sh
+++ Tarea4.1/launch.sh
@@ -1,85 +1,44 @@
 clear;
-echo `date +%s.%3N` > data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 1000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 1500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 2000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 2500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 3000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 3500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 4000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 4500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 1 ./matrixSPMD-PavelNichita.out 5000 40 40 0
-echo `date +%s.%3N` >> data.txt
 
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 1000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 1500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 2000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 2500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 3000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 3500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 4000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 4500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 2 ./matrixSPMD-PavelNichita.out 5000 40 40 0
-echo `date +%s.%3N` >> data.txt
 
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 1000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 1500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 2000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 2500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 3000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 3500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 4000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 4500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 3 ./matrixSPMD-PavelNichita.out 5000 40 40 0
-echo `date +%s.%3N` >> data.txt
 
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 1000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 1500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 2000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 2500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 3000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 3500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 4000 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 4500 40 40 0
-echo `date +%s.%3N` >> data.txt
 mpirun -n 4 ./matrixSPMD-PavelNichita.out 5000 40 40 0
-echo `date +%s.%3N` >> data.txt
