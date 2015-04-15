6b71d4ea188f53359bc04dc0503bf96fc162b322
Pavel <nichitapavel@live.com>
2015-04-16 00:13:23 +0100

    script

---
+++ Tarea8.1/launch.sh
@@ -0,0 +1,89 @@
+clear;
+export OMP_NUM_THREADS=1
+echo "Procesadores 1"
+echo "Tamaño 500"
+./matrizopenmp 500 40 10 0
+echo "Tamaño 1000"
+./matrizopenmp 1000 40 10 0
+echo "Tamaño 1500"
+./matrizopenmp 1500 40 10 0
+echo "Tamaño 2000"
+./matrizopenmp 2000 40 10 0
+echo "Tamaño 2500"
+./matrizopenmp 2500 40 10 0
+echo "Tamaño 3000"
+./matrizopenmp 3000 40 10 0
+echo "Tamaño 3500"
+./matrizopenmp 3500 40 10 0
+echo "Tamaño 4000"
+./matrizopenmp 4000 40 10 0
+echo "Tamaño 4500"
+./matrizopenmp 4500 40 10 0
+echo "Tamaño 5000"
+./matrizopenmp 5000 40 10 0
+export OMP_NUM_THREADS=2
+echo "Procesadores 2"
+echo "Tamaño 500"
+./matrizopenmp 500 40 10 0
+echo "Tamaño 1000"
+./matrizopenmp 1000 40 10 0
+echo "Tamaño 1500"
+./matrizopenmp 1500 40 10 0
+echo "Tamaño 2000"
+./matrizopenmp 2000 40 10 0
+echo "Tamaño 2500"
+./matrizopenmp 2500 40 10 0
+echo "Tamaño 3000"
+./matrizopenmp 3000 40 10 0
+echo "Tamaño 3500"
+./matrizopenmp 3500 40 10 0
+echo "Tamaño 4000"
+./matrizopenmp 4000 40 10 0
+echo "Tamaño 4500"
+./matrizopenmp 4500 40 10 0
+echo "Tamaño 5000"
+./matrizopenmp 5000 40 10 0
+export OMP_NUM_THREADS=3
+echo "Procesadores 3"
+echo "Tamaño 500"
+./matrizopenmp 500 40 10 0
+echo "Tamaño 1000"
+./matrizopenmp 1000 40 10 0
+echo "Tamaño 1500"
+./matrizopenmp 1500 40 10 0
+echo "Tamaño 2000"
+./matrizopenmp 2000 40 10 0
+echo "Tamaño 2500"
+./matrizopenmp 2500 40 10 0
+echo "Tamaño 3000"
+./matrizopenmp 3000 40 10 0
+echo "Tamaño 3500"
+./matrizopenmp 3500 40 10 0
+echo "Tamaño 4000"
+./matrizopenmp 4000 40 10 0
+echo "Tamaño 4500"
+./matrizopenmp 4500 40 10 0
+echo "Tamaño 5000"
+./matrizopenmp 5000 40 10 0
+export OMP_NUM_THREADS=4
+echo "Procesadores 4"
+echo "Tamaño 500"
+./matrizopenmp 500 40 10 0
+echo "Tamaño 1000"
+./matrizopenmp 1000 40 10 0
+echo "Tamaño 1500"
+./matrizopenmp 1500 40 10 0
+echo "Tamaño 2000"
+./matrizopenmp 2000 40 10 0
+echo "Tamaño 2500"
+./matrizopenmp 2500 40 10 0
+echo "Tamaño 3000"
+./matrizopenmp 3000 40 10 0
+echo "Tamaño 3500"
+./matrizopenmp 3500 40 10 0
+echo "Tamaño 4000"
+./matrizopenmp 4000 40 10 0
+echo "Tamaño 4500"
+./matrizopenmp 4500 40 10 0
+echo "Tamaño 5000"
+./matrizopenmp 5000 40 10 0
