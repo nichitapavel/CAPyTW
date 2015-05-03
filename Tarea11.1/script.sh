73b56a8db8bcdb0212864cc55c154fdf8142104d
Pavel <nichitapavel@live.com>
2015-05-04 00:41:29 +0100

    datos finales Desktop + script actualizado

--- Tarea11.1/script.sh
+++ Tarea11.1/script.sh
@@ -1,5 +1,6 @@
 date
 export OMP_NUM_THREADS=2
+echo 'OMP_NUM_THREADS='$OMP_NUM_THREADS
 mpirun -n 1 ./matrix 1000 40 10 0
 mpirun -n 1 ./matrix 2000 40 10 0
 mpirun -n 1 ./matrix 3000 40 10 0
@@ -11,6 +12,7 @@ mpirun -n 2 ./matrix 3000 40 10 0
 mpirun -n 2 ./matrix 4000 40 10 0
 mpirun -n 2 ./matrix 5000 40 10 0
 export OMP_NUM_THREADS=4
+echo 'OMP_NUM_THREADS='$OMP_NUM_THREADS
 mpirun -n 1 ./matrix 1000 40 10 0
 mpirun -n 1 ./matrix 2000 40 10 0
 mpirun -n 1 ./matrix 3000 40 10 0
