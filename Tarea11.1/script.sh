a56d3958fc3a989f5fcae1d7b7c3b14a1812b357
Pavel Linux Laptop <nichitapavel@live.com>
2015-05-04 00:26:44 +0100

    cambios script

--- Tarea11.1/script.sh
+++ Tarea11.1/script.sh
@@ -10,3 +10,9 @@ mpirun -n 2 ./matrix 2000 40 10 0
 mpirun -n 2 ./matrix 3000 40 10 0
 mpirun -n 2 ./matrix 4000 40 10 0
 mpirun -n 2 ./matrix 5000 40 10 0
+export OMP_NUM_THREADS=4
+mpirun -n 1 ./matrix 1000 40 10 0
+mpirun -n 1 ./matrix 2000 40 10 0
+mpirun -n 1 ./matrix 3000 40 10 0
+mpirun -n 1 ./matrix 4000 40 10 0
+mpirun -n 1 ./matrix 5000 40 10 0
