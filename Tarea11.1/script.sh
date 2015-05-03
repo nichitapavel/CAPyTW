95cac1884fecf97965dd343a64d3c0126107f651
Pavel <nichitapavel@live.com>
2015-05-03 23:49:06 +0100

    Tarea 11.1

---
+++ Tarea11.1/script.sh
@@ -0,0 +1,12 @@
+date
+export OMP_NUM_THREADS=2
+./mpirun -n 1 ./matrix 1000 40 10 0
+./mpirun -n 1 ./matrix 2000 40 10 0
+./mpirun -n 1 ./matrix 3000 40 10 0
+./mpirun -n 1 ./matrix 4000 40 10 0
+./mpirun -n 1 ./matrix 5000 40 10 0
+./mpirun -n 2 ./matrix 1000 40 10 0
+./mpirun -n 2 ./matrix 2000 40 10 0
+./mpirun -n 2 ./matrix 3000 40 10 0
+./mpirun -n 2 ./matrix 4000 40 10 0
+./mpirun -n 2 ./matrix 5000 40 10 0
