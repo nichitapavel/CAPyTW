a7f81b577e47a8c6aabe7d0dd431ab3335c3f340
Pavel Linux Laptop <nichitapavel@live.com>
2015-05-03 21:51:41 +0100

    datos laptop

--- Tarea4.1/launch.sh
+++ Tarea4.1/launch.sh
@@ -1,3 +1,43 @@
-clear;
-mpirun -n 4 ./matrixSPMD-PavelNichita.out 1000 10 2 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 500 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 1000 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 1500 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 2000 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 2500 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 3000 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 3500 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 4000 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 4500 40 10 0
+mpirun -n 1 ./matrixSPMD-PavelNichita.out 5000 40 10 0
 
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 500 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 1000 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 1500 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 2000 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 2500 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 3000 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 3500 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 4000 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 4500 40 10 0
+mpirun -n 2 ./matrixSPMD-PavelNichita.out 5000 40 10 0
+
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 500 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 1000 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 1500 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 2000 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 2500 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 3000 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 3500 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 4000 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 4500 40 10 0
+mpirun -n 3 ./matrixSPMD-PavelNichita.out 5000 40 10 0
+
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 500 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 1000 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 1500 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 2000 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 2500 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 3000 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 3500 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 4000 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 4500 40 10 0
+mpirun -n 4 ./matrixSPMD-PavelNichita.out 5000 40 10 0
