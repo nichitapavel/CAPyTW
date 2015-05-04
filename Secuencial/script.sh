6366611c3b417e787df5f232b609532c88ea1fc1
Pavel Linux Laptop <nichitapavel@live.com>
2015-05-04 09:42:52 +0100

    multiplicación matrices secuencial

---
+++ Secuencial/script.sh
@@ -0,0 +1,12 @@
+gcc -o matrix matrix-Secuencial-PavelNichita.c
+date
+./matrix 500 40 10 0
+./matrix 1000 40 10 0
+./matrix 1500 40 10 0
+./matrix 2000 40 10 0
+./matrix 2500 40 10 0
+./matrix 3000 40 10 0
+./matrix 3500 40 10 0
+./matrix 4000 40 10 0
+./matrix 4500 40 10 0
+./matrix 5000 40 10 0
