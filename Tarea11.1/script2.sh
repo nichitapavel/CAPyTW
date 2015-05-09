aea8252f168e2b99a9761ec244ba8fb0242800cc
Pavel Linux Laptop <nichitapavel@live.com>
2015-05-09 19:07:37 +0100

    más datos

--- Tarea11.1/script2.sh
+++ Tarea11.1/script2.sh
@@ -1,6 +1,8 @@
 mpicc -fopenmp -o matrix matrix-MPI-OpenMP-PavelNichita.c
 date
-mpirun -n 1 ./matrix 2000 40 10 0 2
-mpirun -n 1 ./matrix 2000 40 10 0 4
+mpirun -n 2 ./matrix 1000 40 10 0 1
 mpirun -n 2 ./matrix 2000 40 10 0 1
-mpirun -n 4 ./matrix 2000 40 10 0 1
+mpirun -n 2 ./matrix 3000 40 10 0 1
+mpirun -n 2 ./matrix 4000 40 10 0 1
+mpirun -n 2 ./matrix 5000 40 10 0 1
+
